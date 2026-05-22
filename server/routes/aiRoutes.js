14f68ea68d1e14a5476ffb8c1d9f64c786ba5d51
Mohan Reddy <punithreddy678@gmail.com>
2026-05-22 19:37:37 +0530

    fixed gemini model

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
 
     const model =
     genAI.getGenerativeModel({
-      model: "gemini-1.5-flash"
+     model: "gemini-1.5-flash-latest"
     });
 
     const result =
